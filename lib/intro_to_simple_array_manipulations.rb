e890f0c93004f1555064979875773ea7a0de8968
Melissa Wise <wise.melissa19@gmail.com>
2020-02-29 05:46:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,4 +2,18 @@ def using_push(array, string)
   array.push(string)
 end
 
-def
+def using_unshift(array)
+  array.unshift
+end
+
+def shift_with_args(array)
+  array.shift()
+end
+
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
+
+def using_insert(array, element)
+  array.insert(element)
+end
