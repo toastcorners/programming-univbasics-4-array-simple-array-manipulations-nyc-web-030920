b450d366fa69a52385de62573ec196c5e37eb791
Melissa Wise <wise.melissa19@gmail.com>
2020-02-29 06:01:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -16,11 +16,15 @@ def using_pop(array)
 end
 
 def using_pop_with_args(array)
-  array.pop
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift
 end
 
 def shift_with_args(array)
-  array.shift()
+  array.shift(2)
 end
 
 def using_concat(array1, array2)
