1d0b97eb5f12df08e75d22831799e259a70a2de6
Melissa Wise <wise.melissa19@gmail.com>
2020-02-29 05:49:52 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,5 +15,17 @@ def using_concat(array1, array2)
 end
 
 def using_insert(array, element)
-  array.insert(element, 4)
+  array.insert(4, element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, integer)
+  array.
 end
